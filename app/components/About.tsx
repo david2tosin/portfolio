4620bec1410718a9777cfa99520586d7f46451e5
david2tosin <david2tosin@gmail.com>
2024-02-12 10:54:58 +0100

    text fix

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,12 +1,12 @@
-'use client'
+"use client";
 
-import { useInView } from "react-intersection-observer"
-import SectionHeading from "./SectionHeading"
-import { motion } from "framer-motion"
-import { useSectionInView } from "@/lib/hooks"
+import { useInView } from "react-intersection-observer";
+import SectionHeading from "./SectionHeading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 const About = () => {
-    const { ref } = useSectionInView('About')
+  const { ref } = useSectionInView("About");
 
   return (
     <motion.section
@@ -18,29 +18,27 @@ const About = () => {
       ref={ref}
     >
       <SectionHeading>About Me</SectionHeading>
-       <p className="mb-3">
+      <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Electrical Engineering</span>, I decided to pursue my
-        passion for programming. I enrolled in an online coding bootcamp and learned{" "}
+        <span className="font-medium">Electrical Engineering</span>, I decided
+        to pursue my passion for programming. I enrolled in an online coding
+        bootcamp and learnt{" "}
         <span className="font-medium">frontend web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React & Next.js
-        </span>
-        . I am also familiar with TypeScript. I am always looking to
-        learn new technologies. I am currently looking for {" "}
+        is <span className="font-medium">React & Next.js</span>. I am also
+        familiar with TypeScript. I am always looking to learn new technologies.
+        I am currently looking for{" "}
         <span className="font-medium">entry roles</span> as a software
         developer.
       </p>
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching sports and series, and going to the gym.
+        video games, watching sports and TV series, and going to the gym.
       </p>
     </motion.section>
-  )
-}
-export default About
+  );
+};
+export default About;
